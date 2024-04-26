97cd60fce943f36e98c2ca0aac9a61ff96359725
dmquiroga11 <qui24003byui.edu>
2024-04-26 15:52:01 -0500

    This is the loops assigment

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -3,7 +3,33 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep3 World!");
+    {        
+        Random magicalNumber = new Random();
+        int randomNumber = magicalNumber.Next(1,100);
+        int number;
+        string guess;     
+        
+        while (true)
+        {        
+        Console.Write("What is your guess? ");
+         guess = Console.ReadLine();
+         number = int.Parse(guess);        
+        
+        if (number > randomNumber)
+        {
+            Console.WriteLine("Higer");
+        }
+        else if (number < randomNumber)
+        {
+            Console.WriteLine("Lower");
+        }
+        else 
+        {
+            Console.WriteLine("Very well you guessed it!!");
+            break;
+        }
+        }
+
+
     }
 }
