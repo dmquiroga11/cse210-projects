3f17e7ba8c985acfda17345d891fd8d4c0025c1e
dmquiroga11 <qui24003byui.edu>
2024-04-26 17:30:44 -0500

    This is the work of List assigment

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,42 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int>numbers = new List<int>();
+        int number;
+        string nuewNumber;
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        while(true)
+
+        {
+        Console.Write("Enter number: ");
+        string newNumber = Console.ReadLine();
+        number = int.Parse(newNumber);
+
+       
+
+        if (number != 0)
+        {
+            numbers.Add(number);            
+        } 
+        else
+        {
+            int numbersum = numbers.Sum();
+            Console.WriteLine($"The sum is: {numbersum}");
+
+            double averageList = numbers.Average();
+            Console.WriteLine($"The saverage is: {averageList}");
+
+            int maxnumber = numbers.Max();
+            Console.WriteLine($"The sum is: {maxnumber}");
+            break;
+        }     
+        }
     }
 }
