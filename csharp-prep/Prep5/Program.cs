5c2d975108f8726edc4bc875719e71fe7098128c
dmquiroga11 <qui24003byui.edu>
2024-04-26 21:06:54 -0500

    This is the solution for prep 5 assigment

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -1,9 +1,46 @@
 using System;
+using System.Net.Http.Headers;
+using System.Collections.Generic;
+using System.Linq;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome(); 
+        string name = PromptUserName();
+        int number = PromptUserNumber();
+        int squaNumber = SquareNumber(number);                
+        DisplayResult(name , squaNumber);
     }
+
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the Program!");
+    }
+
+    static string PromptUserName()
+    {
+        Console.Write("Please enter your name: ");
+        string name = Console.ReadLine();
+        return name;
+    }
+
+    static int PromptUserNumber()
+    {
+        Console.Write("Please enter your favorite number: ");
+        string favoriteNumber = Console.ReadLine();
+        int number = int.Parse(favoriteNumber);
+        return number;
+    }
+    static int SquareNumber(int number)
+    {
+        int squareNumber = number * number;
+        return squareNumber;
+    }
+    static void DisplayResult(string name, int squaNumber)
+    {
+        Console.WriteLine($"{name}, the square of your number is {squaNumber}");        
+    }
+
 }
