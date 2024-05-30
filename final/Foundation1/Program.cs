7a6a9f027a8e39720de9e3900e113a055096b72c
dmquiroga11 <qui24003byui.edu>
2024-05-30 12:57:45 -0500

    Develop foundation1

--- final/Foundation1/Program.cs
+++ final/Foundation1/Program.cs
@@ -1,9 +1,121 @@
 using System;
+using System.Collections.Generic;
+
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation1 World!");
+        List<Video> videos = new List<Video>();       
+        for (int i = 1; i <= 3; i++)
+        {
+            Video video = new Video();
+            video.SetTitle($"Video {i}");
+            video.SetAuthor($"Author {i}");
+            video.SetDuration(i * 60);
+            video.SetComments(new List<Comment>());
+
+            
+            for (int j = 1; j <= 3; j++)
+            {
+                Comment comment = new Comment();
+                comment.SetCommenterName($"commentator {j}");
+                comment.SetText($"This is the comment {j} in the video {i}");
+
+                video.GetComments().Add(comment);
+            }
+
+            videos.Add(video);
+        }
+      
+        foreach (var video in videos)
+        {
+            Console.WriteLine($"Title: {video.GetTitle()}, Author: {video.GetAuthor()}, Duration: {video.GetDuration()} seconds , Coment Amount : {video.GetCommentCount()}");
+            Console.WriteLine("comments:");
+            foreach (var comment in video.GetComments())
+            {
+                Console.WriteLine($"- {comment.GetCommenterName()}: {comment.GetText()}");
+            }
+            Console.WriteLine();
+        }
     }
 }
+public class Comment
+{
+    private string _commentName;
+    private string _text;
+
+    public string GetCommenterName()
+    {
+        return _commentName;
+    }
+
+    public void SetCommenterName(string value)
+    {
+        _commentName = value;
+    }
+
+    public string GetText()
+    {
+        return _text;
+    }
+
+    public void SetText(string value)
+    {
+        _text = value;
+    }
+}
+
+public class Video
+{
+    private string title;
+    private string author;
+    private int duration;
+    private List<Comment> comments;
+
+    public string GetTitle()
+    {
+        return title;
+    }
+
+    public void SetTitle(string value)
+    {
+        title = value;
+    }
+
+    public string GetAuthor()
+    {
+        return author;
+    }
+
+    public void SetAuthor(string value)
+    {
+        author = value;
+    }
+
+    public int GetDuration()
+    {
+        return duration;
+    }
+
+    public void SetDuration(int value)
+    {
+        duration = value;
+    }
+
+    public List<Comment> GetComments()
+    {
+        return comments;
+    }
+
+    public void SetComments(List<Comment> value)
+    {
+        comments = value;
+    }
+
+    public int GetCommentCount()
+    {
+        return comments.Count;
+    }
+}
+
